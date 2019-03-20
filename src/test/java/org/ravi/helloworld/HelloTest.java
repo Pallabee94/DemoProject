fdeb6a2ca225f57647f936446263f3191c2e5758
Pallabee94 <40536094+Pallabee94@users.noreply.github.com>
2019-03-20 12:48:45 +0530

    Update HelloTest.java

--- src/test/java/org/ravi/helloworld/HelloTest.java
+++ src/test/java/org/ravi/helloworld/HelloTest.java
@@ -19,27 +19,27 @@ public class HelloTest {
 	@BeforeTest
 	public void beforeTest() {
 		
-		System.setProperty("webdriver.chrome.driver","resources\\chromedriver.exe");
+		/*System.setProperty("webdriver.chrome.driver","resources\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.get("http://demoaut.com");
-		driver.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES);
+		driver.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES);*/
 		
 	}
 
 	@Test
 	public void f() {
 
-		String text1 = driver.findElement(By.xpath("//tr[@align = 'right']/td[@colspan = '2']/font[@size = '2' and @face = 'Arial, Helvetica, sans-serif, Verdana']/b")).getText();
+		//String text1 = driver.findElement(By.xpath("//tr[@align = 'right']/td[@colspan = '2']/font[@size = '2' and @face = 'Arial, Helvetica, sans-serif, Verdana']/b")).getText();
 		System.out.println("The date is: "+text1);
-		driver.findElement(By.name("userName")).sendKeys("vishnu");
-		String text = driver.findElement(By.name("userName")).getAttribute("value");
+		/*driver.findElement(By.name("userName")).sendKeys("vishnu");
+		String text = driver.findElement(By.name("userName")).getAttribute("value");*/
 		System.out.println("The Username entered is: "+text);
 
 	}
 
 	@AfterTest
 	public void afterTest() {
-		driver.close();
+		//driver.close();
 	}
 
 
